33c434418958ad191c423147149cbcf5eda5675d
MeiyouMayo <meiyoumayo.1@gmail.com>
2020-10-25 20:11:32 -0500

    refactored ssh library into rust_ec2

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 use rust_ec2::credentials::set_env::set_env_cred_from;
 use rust_ec2::virtual_machine::ec2;
 use rust_ec2::virtual_machine::vm::{VMCore, VMNetwork};
+use rust_ec2::ssh::ssh_agent;
 
 use std::net::TcpStream;
 use std::path::Path;
@@ -12,7 +13,7 @@ use ssh2::Session;
 use std::io::Read;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), ()> {
     let iam_cred_file = File::open("C:/Users/k3nne/Documents/aws/credentials/mc-server/new_user_credentials.csv").unwrap();
     set_env_cred_from(iam_cred_file).await;
 
@@ -20,25 +21,14 @@ async fn main() {
     ec2.status().await;
     ec2.start().await;
 
-    //refactor this into rust_ec2 library
-    let mut ssh_address = ec2.get_pub_ip().await.unwrap();
-    ssh_address.push_str(":22");
+   let ssh = match ssh_agent::SSHAgent::new(&ec2, Path::new("C:/Users/k3nne/Documents/aws/credentials/default/aws-ec2-test.pem")).await {
+       Ok(val) => val,
+       Err(e) => panic!("couldn't make ssh agent!")
+   };
 
-    let tcp = TcpStream::connect(ssh_address).unwrap();
-    let mut sess = Session::new().unwrap();
-    sess.set_tcp_stream(tcp);
-    sess.handshake().unwrap();
-    //TODO needs to authenticate
-    sess.userauth_pubkey_file("ubuntu", None, Path::new("C:/Users/k3nne/Documents/aws/credentials/default/aws-ec2-test.pem"), None).unwrap();
-    assert!(sess.authenticated());
+    println!("{}",ssh.execute("touch this_worked_again").await);
 
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("touch if_this_works_im_gonna_cream_omg").unwrap();
-    let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    println!("{}", s);
-    channel.wait_close();
-    println!("{}", channel.exit_status().unwrap());
+    ec2.stop().await;
 
-    // ec2.stop().await;
+    Ok(())
 }
